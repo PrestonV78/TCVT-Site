5118505c4578ff32b912dc451b52a17a743c45eb
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 17:51:06 -0500

    Socials v28

--- src/components/End.js
+++ src/components/End.js
@@ -4,38 +4,38 @@ export default function End() {
   return (
     <section id="end" className="relative">
       <div className="container mx-auto text-center items-center px-5 py-10">
-        <div className="container mx-auto w-max-full flex flex-row text-center items-center">
+        <div className="container mx-auto ml-1/3 w-max-full flex flex-row text-center items-center">
           <a href="https://www.facebook.com/truckclubatvt/">
             <img
-              class="mx-auto px-5"
+              class="mx-auto"
               alt="fb"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Facebook.webp"
             />
           </a>
           <a href="https://www.linkedin.com/groups/9170257/">
             <img
-              class="mx-auto px-5"
+              class="mx-auto"
               alt="ln"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/LinkedIn.webp"
             />
           </a>
           <a href="https://www.instagram.com/truckclubatvt/">
             <img
-              class="mx-auto px-5"
+              class="mx-auto"
               alt="ig"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Instagram.webp"
             />
           </a>
           <a href="https://www.youtube.com/channel/UCvT9FXcNgF0oYMBc0bdStZw?view_as=subscriber">
             <img
-              class="mx-auto px-5"
+              class="mx-auto"
               alt="yt"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/YouTube.webp"
             />
           </a>
           <a href="https://linktr.ee/truckclubatvt">
             <img
-              class="mx-auto px-5"
+              class="mx-auto"
               alt="link"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Link.webp"
             />
