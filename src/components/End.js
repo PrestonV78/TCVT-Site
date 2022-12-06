dbd9174b240ac51306dc1935e2262d5f3dc5b813
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 16:26:49 -0500

    Socials v9

--- src/components/End.js
+++ src/components/End.js
@@ -9,30 +9,35 @@ export default function End() {
             class="mx-2 max-w-full h-auto"
             alt="fb"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Facebook.webp"
+            href="https://www.facebook.com/truckclubatvt/"
           />
           <img
             class="mx-2 max-w-full h-auto"
             alt="ln"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/LinkedIn.webp"
+            href="https://www.linkedin.com/groups/9170257/"
           />
           <img
             class="mx-2 max-w-full h-auto"
             alt="ig"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Instagram.webp"
+            href="https://www.instagram.com/truckclubatvt/"
           />
           <img
             class="mx-2 max-w-full h-auto"
             alt="yt"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/YouTube.webp"
+            href="https://www.youtube.com/channel/UCvT9FXcNgF0oYMBc0bdStZw?view_as=subscriber"
           />
           <img
             class="mx-2 max-w-full h-auto"
             alt="link"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Link.webp"
+            href="https://linktr.ee/truckclubatvt"
           />
         </div>
         <div className="flex flex-col w-full mb-10">
-          <p className="mb-8 leading-relaxed">
+          <p className="mb-8 leading-relaxed text-black">
             Copyright 2023 Truck Club at Virginia Tech
           </p>
         </div>
