b92e482df5ac2b68b4363838bbe758f2bb5effb3
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 13:34:05 -0500

    About v57

--- src/components/About.js
+++ src/components/About.js
@@ -19,11 +19,30 @@ export default function About() {
           events including meet-ups, tailgates, cruises, and more.
         </p>
       </div>
+      <div className="container h-10 mx-auto flex px-10 py-5 bg-white items-center">
+      </div>
       <img
         class="mx-auto max-w-full h-auto "
         alt="banner"
         src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/banner.webp"
       />
+      <div className="container mx-auto flex md:flex-col flex-col items-center bg-pink-900">
+        <h1 className="title-font px-10 py-5 sm:text-8xl mb-4 text-white font-bold text-center">
+          FEATURED ITEM
+        </h1>
+        <h1 className="title-font px-10 py-5 sm:text-6xl mb-4 text-white font-semibold text-center">
+          TCVT ADOPTS A HIGHWAY
+        </h1>
+        <p className="mb-8 px-10 py-5 sm:text-1xl text-white leading-relaxed text-center">
+          Helping Cleanup 460<br></br><br></br>
+          TCVT has joined the Adopt A Highway program in the state of Virginia! The organization has<br></br>
+          officially adopted Route 460 from Southgate to Tom's Creek in Montgomery County totaling<br></br>
+          3.0 miles. By adopting this section of highway, TCVT agrees to keep it cleared of litter for a<br></br>
+          three-year period by performing at least two successful pickups in one calendar year. We, of<br></br>
+          course, intend to perform more pickups. your support and we are glad that a lot of kids will<br></br>
+          have a lot of awesome toys this Christmas season!
+        </p>
+      </div>
     </section>
   );
 }
