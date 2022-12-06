4705b247482c60e2071bba4ee4501a78117c2f47
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 09:57:07 -0500

    NaveBar Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <header className="bg-purple-800 md:sticky top-0 z-10">
+    <header className="bg-black md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
         <div className="w-20 h-20 ml-1">
           <img
@@ -12,20 +12,36 @@ export default function Navbar() {
             src="../Logo.svg"
           />
         </div>
-        <a className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
-            Preston Verling
-          </a>
-        </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#about" className="ml-3 text-xl">
+            Home
+          </a>
           <a href="#projects" className="mr-5 hover:text-white">
-            Past Projects
+            About/History
           </a>
           <a href="#experience" className="mr-5 hover:text-white">
-            Past Experience
+            Officers
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
-            Skills
+            Calendar/Events
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Services
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Auto Fest
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Sponsors
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Community Action
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            Donate
+          </a>
+          <a href="#skills" className="mr-5 hover:text-white">
+            More
           </a>
         </nav>
         <a
