46366d039c937119dbdee72db9344e224c5148c5
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 12:47:44 -0500

    About v38

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,7 @@ export default function About() {
           events including meet-ups, tailgates, cruises, and more.
         </p>
       </div>
-      <div className="container bg-white items-center">
+      <div className="container bg-white">
         <img
           className="w-full h-auto"
           alt="banner"
