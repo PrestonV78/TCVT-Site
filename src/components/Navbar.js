dff723a447b9566d9db0e6c54b904edccea7acae
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 10:44:43 -0500

    NavBar v22

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function Navbar() {
   return (
     <header className="bg-black md:sticky top-0 z-10">
-      <div className="container mx-auto flex px-5 py-5 md:flex-row flex-col items-center w-80">
+      <div className="container mx-auto flex px-5 py-2 md:flex-row flex-col items-center w-80">
         <img
           className="object-cover object-center rounded"
           alt="badge"
           src="../Logo.svg"
         />
       </div>
-      <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="container px-5 py-2 mx-auto text-center lg:px-40">
         <h1 className="title-font sm:text-5xl text-3xl mb-4 text-white font-bold">
             TRUCK CLUB AT VIRGINIA TECH
         </h1>
