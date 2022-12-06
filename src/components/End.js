bdd5a7c31c69f58ced28ba0b02984a797c45169c
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 16:18:29 -0500

    Socials v5

--- src/components/End.js
+++ src/components/End.js
@@ -6,27 +6,27 @@ export default function End() {
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-row w-full">
           <img
-            class="mx-auto max-w-full h-auto"
+            class="mx-2 max-w-full h-auto"
             alt="fb"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Facebook.webp"
           />
           <img
-            class="mx-auto max-w-full h-auto"
+            class="mx-2 max-w-full h-auto"
             alt="ln"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/LinkedIn.webp"
           />
           <img
-            class="mx-auto max-w-full h-auto"
+            class="mx-2 max-w-full h-auto"
             alt="ig"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Instagram.webp"
           />
           <img
-            class="mx-auto max-w-full h-auto"
+            class="mx-2 max-w-full h-auto"
             alt="yt"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/YouTube.webp"
           />
           <img
-            class="mx-auto max-w-full h-auto"
+            class="mx-2 max-w-full h-auto"
             alt="link"
             src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Link.webp"
           />
