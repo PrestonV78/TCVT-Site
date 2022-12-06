cc9f280a209e8e307bf3ac35559dd193c764264d
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 13:20:23 -0500

    About v52

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ export default function About() {
       </div>
       <div className="container bg-white">
         <img
-          class="max-w-full h-auto object-cover"
+          class="max-w-'103%' h-auto object-cover"
           alt="banner"
           src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/banner.webp"
         />
