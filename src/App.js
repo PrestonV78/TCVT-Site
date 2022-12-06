5f07d2ea39fbd2b0aefee5b4dd3f314d9935b856
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 11:23:45 -0500

    About v1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-import Space from "./components/Space";
-import Experience from "./components/Experience";
 import End from "./components/End";
 
 export default function App() {
@@ -13,13 +9,6 @@ export default function App() {
     <main className="bg-white text-gray-400 body-font">
       <Navbar />
       <About />
-      {/* <Space /> */}
-      <Projects />
-      <Space />
-      <Experience />
-      {/* <Space /> */}
-      <Skills />
-      {/* <Space /> */}
       <Contact />
       <End />
     </main>
--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,18 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-10 md:flex-row flex-col items-center">
       </div>
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-7xl text-3xl mb-4 font-HelloZone text-white">
-            Hi, my name is Preston.
-            <br></br> I build creative solutions.
+        <div className="lg:flex-grow md:w-1 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-5xl text-3xl mb-4 text-amber-600 font-semibold">
+          TRUCK CLUB AT <br></br>
+          VIRGINIA TECH
           </h1>
           <p className="mb-8 text-gray-400 leading-relaxed">
-            I am a current Senior at Virginia Tech studying Computer Science.<br></br>
-            I am set to graduate in May 2023 with my Bachelor's.<br></br>
-            I am currently looking for Post-Grad Full Time Opportunities.<br></br>
+          Welcome to Truck Club at Virginia Tech. We are a student-run organization for people who <br></br>
+          share a passion for trucks. We are here to serve the community and provide a space where <br></br>
+          trucks become a vessel for positive change. We strive to establish a culture of respect, hard <br></br>
+          work, and family among our members. We encourage people of all interests and backgrounds <br></br>
+          to join. You do not need to know about trucks or own one to participate. We will host many <br></br>
+          events including meet-ups, tailgates, cruises, and more.
           </p>
           <div className="flex justify-center">
             <a
@@ -28,13 +31,6 @@ export default function About() {
               See My Past Work
             </a>
           </div>
-        </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="badge"
-            src="../Logo.svg"
-          />
         </div>
       </div>
     </section>
