22eb0561ff0f06a739b6e591a2cdbb8ac6f78a83
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 16:32:56 -0500

    Socials v10

--- src/components/End.js
+++ src/components/End.js
@@ -5,36 +5,41 @@ export default function End() {
     <section id="end" className="relative">
       <div className="container px-5 py-10 mx-auto text-center items-center lg:px-40">
         <div className="w-full items-center flex flex-row">
-          <img
-            class="mx-2 max-w-full h-auto"
-            alt="fb"
-            src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Facebook.webp"
-            href="https://www.facebook.com/truckclubatvt/"
-          />
-          <img
-            class="mx-2 max-w-full h-auto"
-            alt="ln"
-            src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/LinkedIn.webp"
-            href="https://www.linkedin.com/groups/9170257/"
-          />
-          <img
-            class="mx-2 max-w-full h-auto"
-            alt="ig"
-            src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Instagram.webp"
-            href="https://www.instagram.com/truckclubatvt/"
-          />
-          <img
-            class="mx-2 max-w-full h-auto"
-            alt="yt"
-            src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/YouTube.webp"
-            href="https://www.youtube.com/channel/UCvT9FXcNgF0oYMBc0bdStZw?view_as=subscriber"
-          />
-          <img
-            class="mx-2 max-w-full h-auto"
-            alt="link"
-            src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Link.webp"
-            href="https://linktr.ee/truckclubatvt"
-          />
+          <a href="https://www.facebook.com/truckclubatvt/">
+            <img
+              class="mx-2 max-w-full h-auto"
+              alt="fb"
+              src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Facebook.webp"
+            />
+          </a>
+          <a href="https://www.linkedin.com/groups/9170257/">
+            <img
+              class="mx-2 max-w-full h-auto"
+              alt="ln"
+              src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/LinkedIn.webp"
+            />
+          </a>
+          <a href="https://www.instagram.com/truckclubatvt/">
+            <img
+              class="mx-2 max-w-full h-auto"
+              alt="ig"
+              src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Instagram.webp"
+            />
+          </a>
+          <a href="https://www.youtube.com/channel/UCvT9FXcNgF0oYMBc0bdStZw?view_as=subscriber">
+            <img
+              class="mx-2 max-w-full h-auto"
+              alt="yt"
+              src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/YouTube.webp"
+            />
+          </a>
+          <a href="https://linktr.ee/truckclubatvt">
+            <img
+              class="mx-2 max-w-full h-auto"
+              alt="link"
+              src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Link.webp"
+            />
+          </a>
         </div>
         <div className="flex flex-col w-full mb-10">
           <p className="mb-8 leading-relaxed text-black">
