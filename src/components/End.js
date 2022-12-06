a20f99ad83dfc8d44b3e46280960f66b00d6ae61
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 16:40:19 -0500

    Socials v14

--- src/components/End.js
+++ src/components/End.js
@@ -7,35 +7,35 @@ export default function End() {
         <div className="w-full items-center flex flex-row mx-auto text-center items-center">
           <a href="https://www.facebook.com/truckclubatvt/">
             <img
-              class="mx-2 max-w-full h-auto"
+              class="mx-2"
               alt="fb"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Facebook.webp"
             />
           </a>
           <a href="https://www.linkedin.com/groups/9170257/">
             <img
-              class="mx-2 max-w-full h-auto"
+              class="mx-2"
               alt="ln"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/LinkedIn.webp"
             />
           </a>
           <a href="https://www.instagram.com/truckclubatvt/">
             <img
-              class="mx-2 max-w-full h-auto"
+              class="mx-2"
               alt="ig"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Instagram.webp"
             />
           </a>
           <a href="https://www.youtube.com/channel/UCvT9FXcNgF0oYMBc0bdStZw?view_as=subscriber">
             <img
-              class="mx-2 max-w-full h-auto"
+              class="mx-2"
               alt="yt"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/YouTube.webp"
             />
           </a>
           <a href="https://linktr.ee/truckclubatvt">
             <img
-              class="mx-2 max-w-full h-auto"
+              class="mx-2"
               alt="link"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Link.webp"
             />
