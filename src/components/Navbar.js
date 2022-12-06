53aed49803613a6c0996c88bbb8861c6ee5406d2
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 10:00:14 -0500

    NaveBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,15 @@ export default function Navbar() {
   return (
     <header className="bg-black md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
-        <div className="w-20 h-20 ml-1">
           <img
             className="object-cover object-center rounded"
             alt="badge"
             src="../Logo.svg"
           />
-        </div>
+      </div>
+      <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#about" className="ml-3 text-xl">
+          <a href="#about" className="mr-5 hover:text-white">
             Home
           </a>
           <a href="#projects" className="mr-5 hover:text-white">
@@ -44,12 +44,6 @@ export default function Navbar() {
             More
           </a>
         </nav>
-        <a
-          href="#contact"
-          className="inline-flex items-center bg-purple-800 border-0 py-1 px-3 focus:outline-none hover:bg-purple-800 rounded text-base mt-4 md:mt-0 hover:text-white">
-          Contact Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
-        </a>
       </div>
     </header>
   );
