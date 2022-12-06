d6928d88fe56501f1f25d781ec221b03dccb6e30
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 10:01:51 -0500

    NaveBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
-import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Navbar() {
   return (
     <header className="bg-black md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
-          <img
-            className="object-cover object-center rounded"
-            alt="badge"
-            src="../Logo.svg"
-          />
+        <img
+          className="object-cover object-center rounded"
+          alt="badge"
+          src="../Logo.svg"
+        />
       </div>
       <div className="container mx-auto flex flex-wrap p-1 flex-col md:flex-row items-center">
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
