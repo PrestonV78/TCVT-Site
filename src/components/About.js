d8ceea31a35df0143c565518846f11ac4e7c1181
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 12:13:59 -0500

    About v24

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function About() {
   return (
     <section id="about">
-      <div className="container mx-auto flex px-10 py-10 md:flex-col flex-col items-center">
+      <div className="container mx-auto flex px-10 py-15 md:flex-col flex-col items-center">
         <h1 className="title-font sm:text-8xl mb-4 text-pink-900 font-bold text center">
           TRUCK CLUB AT <br></br>
           VIRGINIA TECH
         </h1>
-        <p className="mb-8 sm:text-2xl text-black leading-relaxed text-center">
+        <p className="mb-8 sm:text-1xl text-black leading-relaxed text-center">
           Welcome to Truck Club at Virginia Tech. We are a student-run organization for people who <br></br>
           share a passion for trucks. We are here to serve the community and provide a space where <br></br>
           trucks become a vessel for positive change. We strive to establish a culture of respect, hard <br></br>
@@ -17,7 +17,7 @@ export default function About() {
           events including meet-ups, tailgates, cruises, and more.
         </p>
       </div>
-      <div className="container items-center w-full">
+      <div className="container items-center">
           <img
             className="object-cover object-center max-w-full h-auto"
             alt="banner"
