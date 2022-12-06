8c841b638b33d7ecc9a687494d7cdbf40daa6aaf
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 17:57:06 -0500

    Socials v29

--- src/components/End.js
+++ src/components/End.js
@@ -4,7 +4,7 @@ export default function End() {
   return (
     <section id="end" className="relative">
       <div className="container mx-auto text-center items-center px-5 py-10">
-        <div className="container mx-auto ml-1/3 w-max-full flex flex-row text-center items-center">
+        <div className="container mx-auto w-max-full flex flex-row text-center items-center px-50">
           <a href="https://www.facebook.com/truckclubatvt/">
             <img
               class="mx-auto"
