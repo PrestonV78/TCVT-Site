448f47f8220fad85c7871fe7f795bf8e1c6cd147
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 11:09:26 -0500

    NavBar v35

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,34 +19,34 @@ export default function Navbar() {
       </div>
       <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row justify-between">
         <nav className="md:mr-auto md:ml-4 md:py-1 px-20 md:pl-4 flex flex-wrap justify-between">
-          <a href="#about" className="hover:text-white">
+          <a href="#about" className="mr-5 hover:text-white">
             Home
           </a>
-          <a href="#projects" className="hover:text-white">
+          <a href="#projects" className="mr-5 hover:text-white">
             About/History
           </a>
-          <a href="#experience" className="hover:text-white">
+          <a href="#experience" className="mr-5 hover:text-white">
             Officers
           </a>
-          <a href="#skills" className="hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-white">
             Calendar/Events
           </a>
-          <a href="#skills" className="hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-white">
             Services
           </a>
-          <a href="#skills" className="hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-white">
             Auto Fest
           </a>
-          <a href="#skills" className="hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-white">
             Sponsors
           </a>
-          <a href="#skills" className="hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-white">
             Community Action
           </a>
-          <a href="#skills" className="hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-white">
             Donate
           </a>
-          <a href="#skills" className="hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-white">
             More
           </a>
         </nav>
