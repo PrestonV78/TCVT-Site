a85509827739f3d83ccb05eeb01b6ea67fd066ae
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-06 16:24:44 -0500

    Socials v8

--- src/components/End.js
+++ src/components/End.js
@@ -4,7 +4,7 @@ export default function End() {
   return (
     <section id="end" className="relative">
       <div className="container px-5 py-10 mx-auto text-center items-center lg:px-40">
-        <div className="flex flex-row w-full items-center">
+        <div className="w-full items-center flex flex-row">
           <img
             class="mx-2 max-w-full h-auto"
             alt="fb"
