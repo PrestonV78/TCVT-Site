f1b2e56ebd3dd9b8393ae714da5c87357a1fefac
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-07 08:57:34 -0500

    Socials v41

--- src/components/End.js
+++ src/components/End.js
@@ -4,45 +4,45 @@ export default function End() {
   return (
     <section id="end" className="relative">
       <div className="container mx-auto text-center items-center px-5 py-10">
-        <div className="container mx-auto w-1/5 flex flex-row text-center items-center">
-        <div className="container mx-auto w-3/5 flex flex-row text-center items-center"></div>
+        <div className="container mx-auto w-1/4 flex flex-row text-center items-center">
+        <div className="container mx-auto w-1/2 flex flex-row text-center items-center"></div>
           <a href="https://www.facebook.com/truckclubatvt/">
             <img
-              class="mx-5"
+              class="mx-2"
               alt="fb"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Facebook.webp"
             />
           </a>
           <a href="https://www.linkedin.com/groups/9170257/">
             <img
-              class="mx-5"
+              class="mx-2"
               alt="ln"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/LinkedIn.webp"
             />
           </a>
           <a href="https://www.instagram.com/truckclubatvt/">
             <img
-              class="mx-5"
+              class="mx-2"
               alt="ig"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Instagram.webp"
             />
           </a>
           <a href="https://www.youtube.com/channel/UCvT9FXcNgF0oYMBc0bdStZw?view_as=subscriber">
             <img
-              class="mx-5"
+              class="mx-2"
               alt="yt"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/YouTube.webp"
             />
           </a>
           <a href="https://linktr.ee/truckclubatvt">
             <img
-              class="mx-5"
+              class="mx-2"
               alt="link"
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Link.webp"
             />
           </a>
         </div>
-        <div className="container mx-auto w-1/5 flex flex-row text-center items-center"></div>
+        <div className="container mx-auto w-1/4 flex flex-row text-center items-center"></div>
         <div className="flex flex-col w-full mb-5">
           <p className="mb-8 leading-relaxed text-black">
             Copyright 2023 Truck Club at Virginia Tech
