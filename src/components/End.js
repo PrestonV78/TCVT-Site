7d84a1fb44987c707cf63d057e0755d462736cc2
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-07 09:22:33 -0500

    Socials v55

--- src/components/End.js
+++ src/components/End.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function End() {
   return (
     <section id="end" className="relative">
-      <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row justify-between">
+      <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center">
           <a href="https://www.facebook.com/truckclubatvt/">
             <img
               class="mx-auto"
