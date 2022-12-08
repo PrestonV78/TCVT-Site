b8437f59c8ee2badf1a26b958ac50f2303da585f
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-08 08:59:12 -0500

    Multi?

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Routes, Route, Router } from "react-router-dom";
+
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import End from "./components/End";
@@ -10,14 +12,27 @@ import AutoFest from "./components/AutoFest";
 import Sponsors from "./components/Sponsors";
 import Community from "./components/Community";
 import Donate from "./components/Donate";
-import More from "./components/More"
+import More from "./components/More";
 
 export default function App() {
   return (
     <main className="bg-white text-gray-400 body-font">
-      <Navbar />
-      <Home />
-      <End />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/officers" element={<Officers />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/autofest" element={<AutoFest />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/more" element={<More />} />
+        </Routes>
+        <End />
+      </Router>
     </main>
   );
 }
