d932c4bdf6922e0eb4e7019c5a359cf2ada83161
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-08 17:17:57 -0500

    Officers v3

--- src/components/Officers.js
+++ src/components/Officers.js
@@ -1,31 +1,45 @@
 import React from "react";
 import { officers } from "../data";
 
-
 export default function Officers() {
   return (
-    <section id="officers" className="text-gray-400 bg-white body-font">
+    <section id="officers" className="text-gray-400 bg-pink-900 body-font">
+      <div className="container h-10 mx-auto flex px-10 py-5 bg-white items-center"></div>
+      <img
+        class="mx-auto max-w-full h-auto"
+        alt="banner"
+        src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/Team.webp"
+      />
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+        <h1 className="title-font sm:text-5xl text-2xl mb-4 text-white font-bold text-center">
+        MEET THE TEAM
+        </h1>
+        <p className="mb-8 px-20 sm:text-xl text-md text-white leading-relaxed text-center">
+        The Faces of Our Organization
+        </p>
         <div className="flex flex-wrap -m-4">
           {officers.map((officer) => (
             <a
               href={officer.link}
               key={officer.image}
-              className="sm:w-1/2 w-100 p-4">
+              className="sm:w-1/2 w-100 p-4"
+            >
               <div className="flex relative">
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={officer.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-orange-500 bg-pink-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-orange-500 mb-1">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-white bg-pink-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-white mb-1">
                     {officer.subtitle}
                   </h2>
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     {officer.title}
                   </h1>
-                  <p className="leading-relaxed text-gray-400">{officer.description}</p>
+                  <p className="leading-relaxed text-gray-400">
+                    {officer.description}
+                  </p>
                 </div>
               </div>
             </a>
@@ -33,5 +47,5 @@ export default function Officers() {
         </div>
       </div>
     </section>
-  )
+  );
 }
