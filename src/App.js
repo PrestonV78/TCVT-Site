53998a8791dd0c3e258538cbe5c4e4b791452e1b
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-08 08:53:00 -0500

    Return to Single

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import React from "react";
-
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import End from "./components/End";
-
-import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Officers from "./pages/Officers";
-// import Events from "./pages/Events";
-// import Services from "./pages/Services";
-// import AutoFest from "./pages/AutoFest";
-// import Sponsors from "./pages/Sponsors";
-// import Community from "./pages/Community";
-// import Donate from "./pages/Donate";
-// import More from "./pages/More";
 
 export default function App() {
   return (
