a4f79f64a90751feefcf822c471a6df30fdc1abd
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-08 18:23:03 -0500

    Officers v16

--- src/components/Officers.js
+++ src/components/Officers.js
@@ -20,7 +20,7 @@ export default function Officers() {
         <div className="container h-10 mx-auto flex bg-pink-900 items-center"></div>
         <div className="flex flex-wrap -m-4">
           {officers.map((officer) => (
-            <a href={officer.link} key={officer.image} className="w-1/3 p-4">
+            <a href={officer.link} key={officer.image} className="w-1/3 m-4">
               <div className="flex relative">
                 <img
                   alt="gallery"
@@ -43,7 +43,7 @@ export default function Officers() {
           ))}
         </div>
       </div>
-      <div className="container h-10 mx-auto flex px-10 py-5 bg-white"></div>
+      <div className="container h-10 w-max mx-auto flex px-10 py-5 bg-white"></div>
     </section>
   );
 }
