a595580ff0923c5ea791bdc574d2bd4a8185b07a
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-08 11:33:10 -0500

    About v1

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 export default function About() {
-
-
   return (
     <section id="About" className="relative">
-      <h1 class="font-weight-light">
-        About Page</h1>
+      <div className="container h-10 mx-auto flex px-10 py-15 bg-white items-center"></div>
+      <div className="container mx-auto flex flex-col items-center">
+        <h1 className="title-font py-5 sm:text-7xl text-3xl mb-4 text-pink-900 font-bold text-center">
+          About Us
+        </h1>
+        <p className="mb-8 py-5 sm:text-2xl text-md text-black leading-relaxed text-center">
+          Truck Club at Virginia Tech was founded as a Registered Student
+          Organization at Virginia Tech on October 13, 2020. We have grown from
+          just 5 members to well over 50 members in one year! Our goal is to
+          provide a space where trucks become a vessel for positive change. We
+          strive to accomplish this goal through our events, service, and our
+          TCVT family. We encourage people of all interests and backgrounds to
+          join. If this is what you are looking for, then don't wait and become
+          a member today.
+        </p>
+      </div>
+      <div className="container h-10 mx-auto flex px-10 py-5 bg-white items-center"></div>
+      <img
+        class="mx-auto max-w-full h-auto"
+        alt="banner"
+        src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/trucks.webp"
+      />
     </section>
   );
 }
