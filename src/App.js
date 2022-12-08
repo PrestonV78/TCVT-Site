4af88d2887fedc4b530681cd049a227c5d8ef4ba
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-08 09:05:45 -0500

    Multi? v3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { Routes, Route, Router } from "react-router-dom";
+// import { Routes, Route, Router } from "react-router-dom";
 
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import End from "./components/End";
-import About from "./components/About";
-import Officers from "./components/Officers";
-import Events from "./components/Events";
-import Services from "./components/Services";
-import AutoFest from "./components/AutoFest";
-import Sponsors from "./components/Sponsors";
-import Community from "./components/Community";
-import Donate from "./components/Donate";
-import More from "./components/More";
+// import About from "./components/About";
+// import Officers from "./components/Officers";
+// import Events from "./components/Events";
+// import Services from "./components/Services";
+// import AutoFest from "./components/AutoFest";
+// import Sponsors from "./components/Sponsors";
+// import Community from "./components/Community";
+// import Donate from "./components/Donate";
+// import More from "./components/More";
 
 export default function App() {
   return (
     <main className="bg-white text-gray-400 body-font">
-      <Router>
+      {/* <Router> */}
         <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
+        <Home />
+        {/* <Routes> */}
+          {/* <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/officers" element={<Officers />} />
           <Route path="/events" element={<Events />} />
@@ -29,10 +30,10 @@ export default function App() {
           <Route path="/sponsors" element={<Sponsors />} />
           <Route path="/community" element={<Community />} />
           <Route path="/donate" element={<Donate />} />
-          <Route path="/more" element={<More />} />
-        </Routes>
+          <Route path="/more" element={<More />} /> */}
+        {/* </Routes> */}
         <End />
-      </Router>
+      {/* </Router> */}
     </main>
   );
 }
