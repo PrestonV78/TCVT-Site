5a6370bc972d82f98d4850500704db09bc9ad3b1
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2022-12-08 08:45:11 -0500

    Multipage v17

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Routes, Route, Router } from "react-router-dom";
 
-// import Navbar from "./components/Navbar";
-// import End from "./components/End";
+import Navbar from "./components/Navbar";
+import End from "./components/End";
 
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Officers from "./pages/Officers";
-import Events from "./pages/Events";
-import Services from "./pages/Services";
-import AutoFest from "./pages/AutoFest";
-import Sponsors from "./pages/Sponsors";
-import Community from "./pages/Community";
-import Donate from "./pages/Donate";
-import More from "./pages/More";
+// import About from "./pages/About";
+// import Officers from "./pages/Officers";
+// import Events from "./pages/Events";
+// import Services from "./pages/Services";
+// import AutoFest from "./pages/AutoFest";
+// import Sponsors from "./pages/Sponsors";
+// import Community from "./pages/Community";
+// import Donate from "./pages/Donate";
+// import More from "./pages/More";
 
 function App() {
   return (
