7bdbd18022b4068aabb7c526a26936d61ae851ef
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-24 16:33:19 -0500

    Officer Sizing 3

--- src/components/Officers.js
+++ src/components/Officers.js
@@ -20,7 +20,7 @@ export default function Officers() {
         <div className="container h-10 mx-auto flex bg-pink-900 items-center"></div>
         <div className="flex flex-wrap -m-4">
           {officers.map((officer) => (
-            <a href={officer.link} key={officer.image} className="md:w-1/3 w-full h-full p-4">
+            <a href={officer.link} key={officer.image} className="md:w-1/3 p-4">
               <div className="flex relative">
                 <img
                   alt="gallery"
