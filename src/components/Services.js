e50a6ef83d5898268c2bcc95868520c34f986277
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-25 14:58:38 -0500

    Services Page 2

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Services() {
   return (
     <section id="Services" className="relative">
-      <div className="container h-10 mx-auto flex px-10 py-15 bg-white items-center"></div>
+      <div className="container h-10 mx-auto flex px-10 py-15 bg-pink-900 items-center"></div>
       <div className="container mt-5 mx-auto flex flex-col items-center">
         <h1 className="title-font sm:text-5xl text-2xl mb-4 text-pink-900 font-bold text-center">
           TCVT SERVICES
@@ -27,6 +27,20 @@ export default function Services() {
           2. General Moving Assistance Services: If you are in need of assistance
           to move furniture, personal items, mulch, or anything at all, then contact
           us so that we can have our members and our trucks come to your assistance.
+        </p>
+        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
+        <p className="mb-8 px-20 text-md text-white font-semibold text-center">
+          All services are individualized and customized to each request. Service availability
+          may change due to weather, availability, resources, and more. Before every service,
+          TCVT will contact you to confirm and remind you of said service that is scheduled to
+          happen. <strong> If you are a TCVT Sponsor, then you have priority status to these services. </strong>
+          These services are completely free but we do appreciate any donations to TCVT.
+        </p>
+        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
+        <p className="mb-8 px-20 text-md text-white font-semibold text-center">
+          If you are interested, then please contact any of the officers or email us at
+          <strong>truckclubatvt@gmail.com</strong>. Please fill out the optional survey
+          below if you would like to give us more information about your service.
         </p>
         <div className="container h-10 mx-auto flex px-10 py-5 "></div>
       </div>
