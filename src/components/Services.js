e4aa4fe11ffb7078f4ae0653625b6d47a7f9f8c5
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-25 16:02:03 -0500

    Services Page 31

--- src/components/Services.js
+++ src/components/Services.js
@@ -70,7 +70,8 @@ export default function Services() {
               src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/pallet.webp"
             />
           </div>
-          <div className="container w-1/2  flex-col flex content-center items-center px-10">
+          <div className="container w-1/2 flex-col flex content-center items-center px-10">
+            <div className="container h-2 mx-auto flex px-10 py-5 "></div>
             <h1 className="title-font sm:text-4xl text-2xl text-white font-bold text-center">
               PALLET PICKUP
             </h1>
