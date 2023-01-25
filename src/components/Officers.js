2aa8a685baaf3295161453cebefc764a222c03b1
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-25 11:05:55 -0500

    Officer Page 7

--- src/components/Officers.js
+++ src/components/Officers.js
@@ -15,7 +15,7 @@ export default function Officers() {
         <div className="container h-10 mx-auto flex bg-pink-900 items-center"></div>
         <div className="flex flex-wrap -m-4">
           {officers.map((officer) => (
-            <a href={officer.link} key={officer.image} className="h-full w-full p-4">
+            <a href={officer.link} key={officer.image} className="h-auto w-1/3 p-4">
               <div className="flex relative">
                 <img
                   alt="gallery"
