ac36b405cd02e495fa692c7f03b70b91fa69e14a
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-25 15:03:41 -0500

    Services Page 4

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Services() {
   return (
     <section id="Services" className="relative">
-      <div className="container h-10 mx-auto flex py-15 bg-pink-900 items-center"></div>
+      <div className="container h-10 x-full flex py-15 bg-pink-900 items-center"></div>
       <div className="container mt-5 mx-auto flex flex-col items-center">
         <h1 className="title-font sm:text-5xl text-2xl mb-4 text-pink-900 font-bold text-center">
           TCVT SERVICES
@@ -12,23 +12,23 @@ export default function Services() {
           Our mission at Truck Club at Virginia Tech is to use our trucks as a vessel for positive change,
           which is why we want to help you in any way we can!
         </h1>
-        <div className="container h-10 mx-auto flex py-15 bg-pink-900 items-center"></div>
+        <div className="container h-3 x-full flex py-15 bg-pink-900 items-center"></div>
         <p className="mb-8 px-20 text-md text-black leading-relaxed text-center">
           Currently, TCVT is providing 2 services to assist the community around us.
         </p>
-        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
+        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
         <p className="mb-8 px-20 text-md text-black font-semibold text-center">
           1. Pallet Pickup Services: If you have wooden pallets that are laying around
           or are going to the landfill, then we can help take that inconvenience off your
           hands. TCVT reuses these pallets for storage, woodwork, bonfires, and more.
         </p>
-        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
+        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
         <p className="mb-8 px-20 text-md text-black font-semibold text-center">
           2. General Moving Assistance Services: If you are in need of assistance
           to move furniture, personal items, mulch, or anything at all, then contact
           us so that we can have our members and our trucks come to your assistance.
         </p>
-        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
+        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
         <p className="mb-8 px-20 text-md text-black font-semibold text-center">
           All services are individualized and customized to each request. Service availability
           may change due to weather, availability, resources, and more. Before every service,
@@ -36,10 +36,10 @@ export default function Services() {
           happen. <strong> If you are a TCVT Sponsor, then you have priority status to these services. </strong>
           These services are completely free but we do appreciate any donations to TCVT.
         </p>
-        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
+        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
         <p className="mb-8 px-20 text-md text-black font-semibold text-center">
           If you are interested, then please contact any of the officers or email us at
-          <strong>truckclubatvt@gmail.com</strong>. Please fill out the optional survey
+          <strong> truckclubatvt@gmail.com</strong>. Please fill out the optional survey
           below if you would like to give us more information about your service.
         </p>
         <div className="container h-10 mx-auto flex px-10 py-5 "></div>
