6e0c3d680bbcece0f77ca5fb1ea97217ec8cb503
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-25 15:22:41 -0500

    Services Page 13

--- src/components/Services.js
+++ src/components/Services.js
@@ -8,7 +8,7 @@ export default function Services() {
         <h1 className="title-font sm:text-5xl text-2xl mb-4 text-pink-900 font-bold text-center">
           TCVT SERVICES
         </h1>
-        <div className="container mx-auto flex px-10 py-5 ">
+        <div className="container mx-auto flex flex-col px-10 py-5 ">
           <h1 className="title-font px-35 mx-35 sm:text-2xl text-lg mb-4 text-black font-semibold text-center ">
             Our mission at Truck Club at Virginia Tech is to use our trucks as a vessel for positive change,
             which is why we want to help you in any way we can!
