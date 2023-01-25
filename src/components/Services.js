2c0a7135bf6028dcd2b58da1d678ddc0e10ac6de
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-25 15:11:04 -0500

    Services Page 7

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,32 +12,27 @@ export default function Services() {
           Our mission at Truck Club at Virginia Tech is to use our trucks as a vessel for positive change,
           which is why we want to help you in any way we can!
         </h1>
-        <div className="container h-5 w-full flex py-15 bg-pink-900 items-center"></div>
-        <p className="px-35 text-md text-black leading-relaxed text-center">
+        <p className="px-35 text-md text-black leading-relaxed mb-4 text-center">
           Currently, TCVT is providing 2 services to assist the community around us.
         </p>
-        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
-        <p className="px-35 text-md text-black font-semibold text-center">
+        <p className="px-35 text-md text-black font-semibold mb-4 text-center">
           1. Pallet Pickup Services: If you have wooden pallets that are laying around
           or are going to the landfill, then we can help take that inconvenience off your
           hands. TCVT reuses these pallets for storage, woodwork, bonfires, and more.
         </p>
-        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
-        <p className="px-35 text-md text-black font-semibold text-center">
+        <p className="px-35 text-md text-black font-semibold mb-4 text-center">
           2. General Moving Assistance Services: If you are in need of assistance
           to move furniture, personal items, mulch, or anything at all, then contact
           us so that we can have our members and our trucks come to your assistance.
         </p>
-        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
-        <p className="px-35 text-md text-black leading-relaxed text-center">
+        <p className="px-35 text-md text-black leading-relaxed mb-4 text-center">
           All services are individualized and customized to each request. Service availability
           may change due to weather, availability, resources, and more. Before every service,
           TCVT will contact you to confirm and remind you of said service that is scheduled to
           happen. <strong> If you are a TCVT Sponsor, then you have priority status to these services. </strong>
           These services are completely free but we do appreciate any donations to TCVT.
         </p>
-        <div className="container h-3 mx-auto flex px-10 py-5 "></div>
-        <p className="px-35 text-md text-black leading-relaxed text-center">
+        <p className="px-35 text-md text-black leading-relaxed mb-4 text-center">
           If you are interested, then please contact any of the officers or email us at
           <strong> truckclubatvt@gmail.com</strong>. Please fill out the optional survey
           below if you would like to give us more information about your service.
