9d095791e0686b5697937601e3008a2c20b9ce2c
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-26 16:50:17 -0500

    Sponsors Page 3

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -8,14 +8,14 @@ export default function Sponsors() {
       <h1 className="title-font sm:text-6xl text-3xl text-pink-900 font-bold text-center">
         BECOME A SPONSOR
       </h1>
-      <h1 className="title-font px-35 mx-35 sm:text-4xl text-2xl mb-8 text-black font-semibold text-center ">
+      <h1 className="title-font w-2/3 px-35 mx-auto sm:text-4xl text-2xl mb-8 text-black font-semibold text-center ">
         We are so happy you’re interested in getting involved with our work here at Truck Club at Virginia Tech.
         There are so many ways for you to help, and we truly appreciate each and every effort. By lending your
         support, you’ll become a valuable part of our Non-Profit Organization and help to strengthen our operations.
       </h1>
       <div className="container h-30 mx-auto flex px-10 py-5 "></div>
       <div className="container w-full flex flex-col bg-white ">
-        <div className="container mx-auto w-2/3 flex flex-row bg-pink-900 py-5 ">
+        <div className="container mx-auto w-3/4 flex flex-row bg-pink-900 py-5 ">
           <div className="container w-1/2 flex px-10 py-5 ">
             <img
               class="mx-auto max-w-full h-auto"
@@ -38,9 +38,9 @@ export default function Sponsors() {
           </div>
         </div>
 
-        <div className="container h-10 mx-auto flex bg-white px-10 py-5 "></div>
+        <div className="container h-5 mx-auto flex bg-white px-10 py-5 "></div>
 
-        <div className="container mx-auto w-2/3 flex flex-row bg-pink-900 py-5 ">
+        <div className="container mx-auto w-3/4 flex flex-row bg-pink-900 py-5 ">
           <div className="container w-1/2 flex px-10 py-5 ">
             <img
               class="mx-auto max-w-full h-auto"
