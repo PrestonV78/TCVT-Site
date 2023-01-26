7f06923190cd880f5b897d556c7f8504815aaec9
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-26 16:25:47 -0500

    Navbar Logo 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const linkStyle = {
 export default function Navbar() {
   return (
     <header className="bg-black md: top-5 z-10">
-      <div className="container mx-auto flex w-2/5 py-2 flex-row items-center">
+      <div className="container mx-auto flex w-1/3 py-2 flex-row items-center">
         <img
           className="object-center object-cover rounded"
           alt="badge"
