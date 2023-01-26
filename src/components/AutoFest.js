e4b4a00e919a40dbcab236a76a47edde10d840c1
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-26 15:59:32 -0500

    AutoFest Page 6

--- src/components/AutoFest.js
+++ src/components/AutoFest.js
@@ -26,34 +26,53 @@ export default function AutoFest() {
         <h1 className="title-font sm:text-3xl text-xl mb-4 text-white font-semibold text-center ">
           Next Show, November 2023
         </h1>
-        <div className="container h-5 mx-auto flex px-10 py-5 "></div>
 
         <div className="container mx-auto flex-row flex px-10 py-5 ">
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
             <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Attendee Regestration
             </h1>
+            <a
+              href=""
+              className=" mx-auto w-1/3 inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-lg">
+              <strong>Coming Soon</strong>
+            </a>
           </div>
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
             <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Vendor Regestration
             </h1>
+            <a
+              href=""
+              className=" mx-auto w-1/3 inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-lg">
+              <strong>Coming Soon</strong>
+            </a>
           </div>
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
             <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Sponsor Regestration
             </h1>
+            <a
+              href=""
+              className=" mx-auto w-1/3 inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-lg">
+              <strong>Coming Soon</strong>
+            </a>
           </div>
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
             <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Shop Merch
             </h1>
+            <a
+              href=""
+              className=" mx-auto w-1/3 inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-lg">
+              <strong>Coming Soon</strong>
+            </a>
           </div>
         </div>
       </div>
 
       <div className="container h-10 mx-auto flex-col flex px-10 py-15 bg-white items-center">
-      <div className="container h-10 mx-auto flex px-10 py-5 "></div>
+        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
         <h1 className="title-font px-10 sm:text-3xl text-xl mb-4 text-pink-900 font-semibold text-center ">
           Thank You For An Awesome Auto Fest 2022
         </h1>
