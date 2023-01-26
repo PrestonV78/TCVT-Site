c7a18f56c408296af072dce53f7e6438f74be73d
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-26 15:55:57 -0500

    AutoFest Page 4

--- src/components/AutoFest.js
+++ src/components/AutoFest.js
@@ -26,33 +26,34 @@ export default function AutoFest() {
           Next Show, November 2023
         </h1>
         <div className="container h-10 mx-auto flex px-10 py-5 "></div>
-        <div className="container mx-auto flex-row flex px-10 py-5 ">
 
+        <div className="container mx-auto flex-row flex px-10 py-5 ">
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
-            <h1 className="title-font sm:text-3xl text-xl mb-4 text-white font-semibold text-center ">
+            <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Attendee Regestration
             </h1>
           </div>
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
-            <h1 className="title-font sm:text-3xl text-xl mb-4 text-white font-semibold text-center ">
+            <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Vendor Regestration
             </h1>
           </div>
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
-            <h1 className="title-font sm:text-3xl text-xl mb-4 text-white font-semibold text-center ">
+            <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Sponsor Regestration
             </h1>
           </div>
           <div className="container mx-auto flex-col flex px-10 py-5 border-4 border-white">
-            <h1 className="title-font sm:text-3xl text-xl mb-4 text-white font-semibold text-center ">
+            <h1 className="title-font sm:text-3xl text-xl mx-4 mb-4 text-white font-semibold text-center ">
               Shop Merch
             </h1>
           </div>
-
         </div>
+        
         <div className="container h-10 mx-auto flex px-10 py-5 "></div>
       </div>
-      <div className="container h-10 mx-auto flex-row flex px-10 py-15 bg-white items-center">
+
+      <div className="container h-10 mx-auto flex-col flex px-10 py-15 bg-white items-center">
         <h1 className="title-font px-10 sm:text-3xl text-xl mb-4 text-pink-900 font-semibold text-center ">
           Thank You For An Awesome Auto Fest 2022
         </h1>
