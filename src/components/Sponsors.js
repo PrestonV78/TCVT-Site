a87e07accd6813a45f662044e87b943f43370bd0
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-26 16:53:58 -0500

    Sponsors Page 5

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -15,7 +15,8 @@ export default function Sponsors() {
       </h1>
       <div className="container h-30 mx-auto flex px-10 py-5 "></div>
       <div className="container w-full flex flex-col bg-white ">
-        <div className="container mx-auto w-3/4 flex flex-row bg-pink-900 py-5 ">
+        
+        <div className="container mx-auto w-4/5 flex flex-row bg-pink-900 py-5 ">
           <div className="container w-1/2 flex px-10 py-5 ">
             <img
               class="mx-auto max-w-full h-auto"
@@ -40,7 +41,7 @@ export default function Sponsors() {
 
         <div className="container h-5 mx-auto flex bg-white px-10 py-5 "></div>
 
-        <div className="container mx-auto w-3/4 flex flex-row bg-pink-900 py-5 ">
+        <div className="container mx-auto w-4/5 flex flex-row bg-pink-900 py-5 ">
           <div className="container w-1/2 flex px-10 py-5 ">
             <img
               class="mx-auto max-w-full h-auto"
@@ -66,7 +67,7 @@ export default function Sponsors() {
         <div className="container h-10 mx-auto flex bg-white px-10 py-5 "></div>
       </div>
 
-      <div className="container mx-auto flex flex-col bg-pink-900 py-5 ">
+      <div className="container w-full mx-auto flex flex-col bg-pink-900 py-5 ">
         <h1 className="title-font sm:text-6xl text-3xl text-white font-bold text-center">
           SPONSORSHIP PAYMENT
         </h1>
