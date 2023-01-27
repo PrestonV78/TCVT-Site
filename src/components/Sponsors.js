cf94d11a256a45b05576f20be6442c3c5ff9cde7
PrestonV78 <90864596+PrestonV78@users.noreply.github.com>
2023-01-26 20:37:23 -0500

    Sponsors Page 16

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -4,53 +4,55 @@ import { officers } from "../data";
 export default function Sponsors() {
   return (
     <section id="Sponsors" className="relative">
-      <div className="container h-30 mx-auto flex  py-5 "></div>
-      <h1 className="title-font sm:text-6xl text-3xl text-pink-900 font-bold text-center">
-        BECOME A SPONSOR
-      </h1>
-      <div className="container h-5 mx-auto flex  py-5 "></div>
-      <h1 className="title-font w-2/3 px-35 mx-auto sm:text-2xl text-lg mb-8 text-black font-semibold text-center ">
-        We are so happy you’re interested in getting involved with our work here at Truck Club at Virginia Tech.
-        There are so many ways for you to help, and we truly appreciate each and every effort. By lending your
-        support, you’ll become a valuable part of our Non-Profit Organization and help to strengthen our operations.
-      </h1>
-      <div className="container h-30 mx-auto flex  py-5 "></div>
+      <div className="container w-full flex-col flex">
+        <div className="container h-30 mx-auto flex px-10 py-5 "></div>
+        <h1 className="title-font sm:text-6xl text-3xl text-pink-900 font-bold text-center">
+          BECOME A SPONSOR
+        </h1>
+        <div className="container h-5 mx-auto flex px-10 py-5 "></div>
+        <h1 className="title-font w-2/3 px-35 mx-auto sm:text-2xl text-lg mb-8 text-black font-semibold text-center ">
+          We are so happy you’re interested in getting involved with our work here at Truck Club at Virginia Tech.
+          There are so many ways for you to help, and we truly appreciate each and every effort. By lending your
+          support, you’ll become a valuable part of our Non-Profit Organization and help to strengthen our operations.
+        </h1>
+        <div className="container h-30 mx-auto flex px-10 py-5 "></div>
 
-      <div className="container w-full flex flex-row bg-white ">
-        <div className="container h-5 mx-auto flex bg-white  py-5 "></div>
-        <div className="container mx-auto w-5/6 flex-row flex bg-pink-900 py-5 ">
-          <div className="container w-1/2 flex  py-5 ">
-            <img
-              class="mx-auto max-w-full h-auto"
-              alt="moving"
-              src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/handshake.webp"
-            />
-          </div>
-          <div className="container w-1/2 flex-col flex  py-5 ">
-            <h1 className="title-font px-35 mx-35 sm:text-4xl text-2xl mb-8 text-white font-semibold text-center ">
-              <strong>BECOME A SPONSOR</strong>
-            </h1>
-            <h1 className="title-font px-35 mx-35 sm:text-4xl text-2xl mb-8 text-white leading-relaxed text-center ">
-              Have an Impact
-            </h1>
-            <a
-              href="https://forms.gle/QSobfhRWdqkX8Wi49"
-              className=" mx-auto inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-sm">
-              <strong>Apply</strong>
-            </a>
+        <div className="container w-full flex flex-col bg-white ">
+          <div className="container h-5 mx-auto flex bg-white px-10 py-5 "></div>
+          <div className="container mx-auto w-5/6 flex flex-row bg-pink-900 py-5 ">
+            <div className="container w-1/2 flex px-10 py-5 ">
+              <img
+                class="mx-auto max-w-full h-auto"
+                alt="moving"
+                src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/handshake.webp"
+              />
+            </div>
+            <div className="container w-1/2 flex-col flex px-10 py-5 ">
+              <h1 className="title-font px-35 mx-35 sm:text-4xl text-2xl mb-8 text-white font-semibold text-center ">
+                <strong>BECOME A SPONSOR</strong>
+              </h1>
+              <h1 className="title-font px-35 mx-35 sm:text-4xl text-2xl mb-8 text-white leading-relaxed text-center ">
+                Have an Impact
+              </h1>
+              <a
+                href="https://forms.gle/QSobfhRWdqkX8Wi49"
+                className=" mx-auto inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-sm">
+                <strong>Apply</strong>
+              </a>
+            </div>
           </div>
 
-          <div className="container h-5 mx-auto flex bg-white  py-5 "></div>
+          <div className="container h-5 mx-auto flex bg-white px-10 py-5 "></div>
 
           <div className="container mx-auto w-5/6 flex flex-row bg-pink-900 py-5 ">
-            <div className="container w-1/2 flex  py-5 ">
+            <div className="container w-1/2 flex px-10 py-5 ">
               <img
                 class="mx-auto max-w-full h-auto"
                 alt="moving"
                 src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/meeting.webp"
               />
             </div>
-            <div className="container w-1/2 flex-col flex  py-5 ">
+            <div className="container w-1/2 flex-col flex px-10 py-5 ">
               <h1 className="title-font px-35 mx-35 sm:text-4xl text-2xl mb-8 text-white font-semibold text-center ">
                 <strong>SPONSORSHIP PACKAGE</strong>
               </h1>
@@ -65,7 +67,7 @@ export default function Sponsors() {
             </div>
           </div>
 
-          <div className="container h-10 mx-auto flex bg-white  py-5 "></div>
+          <div className="container h-10 mx-auto flex bg-white px-10 py-5 "></div>
         </div>
 
         <div className="container w-full mx-auto flex flex-col bg-pink-900">
@@ -86,11 +88,11 @@ export default function Sponsors() {
         </div>
 
         <div className="container w-full flex flex-col bg-white ">
-          <div className="container h-10 mx-auto flex bg-white  py-5 "></div>
+          <div className="container h-10 mx-auto flex bg-white px-10 py-5 "></div>
           <h1 className="title-font sm:text-6xl text-3xl text-black font-bold text-center">
             Package List and Pay Link
           </h1>
-          <div className="container h-10 mx-auto flex bg-white  py-5 "></div>
+          <div className="container h-10 mx-auto flex bg-white px-10 py-5 "></div>
         </div>
 
         <div className="container mx-auto flex flex-col bg-pink-900 py-5 ">
