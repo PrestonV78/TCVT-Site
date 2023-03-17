ac28164eaba35e82ff2df345f9dad4a55fb6d1cd
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2023-03-17 10:01:40 -0400

    Home Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -53,39 +53,7 @@ export default function Home() {
         alt="banner"
         src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/banner.webp"
       />
-      <div className="container py-30 mx-auto flex-col items-center bg-pink-900 max-w-full">
-        <div className="container h-20 mx-auto flex px-10 py-5 "></div>
-        <h1 className="title-font px-10 sm:text-6xl text-3xl mb-4 text-white font-bold text-center ">
-          FEATURED ITEM
-        </h1>
-        <div className="container h-20 mx-auto flex px-10 py-5 "></div>
-        <h1 className="title-font px-10 sm:text-5xl text-3xl mb-4 text-white font-semibold text-center ">
-          TCVT ADOPTS A HIGHWAY
-        </h1>
-        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
-        <p className="mb-8 px-20 sm:text-2xl text-lg text-white leading-relaxed text-center">
-          Helping Cleanup 460<br></br>
-          <br></br>
-          TCVT has joined the Adopt A Highway program in the state of Virginia!
-          The organization has
-          officially adopted Route 460 from Southgate to Tom's Creek in
-          Montgomery County totaling
-          3.0 miles. By adopting this section of highway, TCVT agrees to keep it
-          cleared of litter for a
-          three-year period by performing at least two successful pickups in one
-          calendar year. We, of
-          course, intend to perform more pickups. your support and we are glad
-          that a lot of kids will
-          have a lot of awesome toys this Christmas season!
-        </p>
-        <div className="container h-10 mx-auto flex px-10 py-5 "></div>
-        <img
-          class="mx-auto w-1/2 h-auto "
-          alt="banner"
-          src="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/highway.webp"
-        />
-        <div className="container h-40 mx-auto flex px-10 py-5 "></div>
-      </div>
+      <div className="container py-30 mx-auto flex-col items-center bg-pink-900 max-w-full"></div>
       <div className="container mx-auto flex flex-wrap px-10 py-20">
         <form
           netlify
