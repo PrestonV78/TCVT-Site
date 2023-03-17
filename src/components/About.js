d9f29a24bec313ae315bb5f30cb37eaba4d9584a
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2023-03-17 09:29:27 -0400

    History Update

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ export default function About() {
           strive to accomplish this goal through our events, service, and our
           TCVT family. We encourage people of all interests and backgrounds to
           join. If this is what you are looking for, then don't wait and become
-          a member today.
+          a member today!
         </p>
       </div>
       <div className="container h-10 mx-auto flex px-10 py-5 bg-white items-center"></div>
@@ -36,32 +36,18 @@ export default function About() {
         </h1>
         <div className="container h-10 mx-auto flex px-10 py-5 "></div>
         <p className="mb-8 px-20 text-md text-white leading-relaxed text-center">
-          Truck Club at Virginia Tech started off as an idea from Sebastian
-          Buenaño. As a sophomore, Sebastian had already been part of multiple
-          organizations and clubs on campus but he always dreamed about starting
-          his own club at Virginia Tech especially a truck club. Sebastian has a
-          passion for trucks and he himself has a 2015 GMC Sierra Denali 1500
-          which he calls "Big Red". During the Spring 2020 Semester, Sebastian
-          would share his vision and passion with some close friends. Many of
-          those close friends like Tara Tafro, Leo Olivera, Reagan Sanchez,
-          Jerry Gonzalez, Jhosselin Vasquez, Eric Aponte, and Wayne Thompson all
-          encouraged Sebastian to start a club. His friends also gave him ideas
-          and tips about making the club. As Sebastian's passion grew he started
-          to see the awesome potential of forming a possible Truck Club on
-          campus. Southwestern Virginia and Virginia Tech itself is definitely a
-          hotspot for trucks and truck enthusiasts which is why Sebastian knew
-          his vision could come to reality. With the support from family and
-          friends, the process of making a truck club at Virginia Tech was
-          underway in the late Spring 2020 Semester. By early Fall 2020 Semester
-          Sebastian was able to form a group of 5 founding Truck Club at
-          Virginia Tech (TCVT) members. Reagan Sanchez as Social Media Chair,
-          Jerry Gonzalez as Secretary, Jhosselin Vasquez as Treasure, Wayne
-          Thompson as Vice President, and Sebastian Buenaño as President were
-          the founding members of TCVT. After finalizing all the necessary steps
-          to becoming a Registered Student Organization (RSO) Truck Club at
-          Virginia Tech was officially recognized by Virginia Tech on October
-          13, 2020! This is our history and we cannot wait to see what the
-          future holds for this awesome organization!
+          Truck Club at Virginia Tech was founded in 2020 by a small group of close 
+          friends as a way to express themselves through their vehicles and create 
+          a positive outreach to other members of the Hokie Nation. The original founding 
+          members, Sebastian Buenaño, Wayne Thompson, Reagan Sanchez, Jerry Gonzalez, and 
+          Jhosselin Vasquez, believed that they could combine their passion for Trucks with 
+          the Virginia Tech motto of "Ut Prosim" (That I May Serve), and thus the club motto 
+          "Using Our Trucks as a Vessel for Positive Change" was born. Truck Club serves our 
+          motto as well as the Virginia Tech motto by creating and serving multiple charity 
+          events every year . With these values held dearly by our Club, the officers and 
+          members, past and present, of Truck Club at Virginia Tech hope to continue serving 
+          our community for many years to come.
+
         </p>
         <div className="container h-10 mx-auto flex px-10 py-5 "></div>
         <img
