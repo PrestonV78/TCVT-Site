129471e72463d0b1d2b178e40840760118bc9059
Preston Verling <90864596+PrestonV78@users.noreply.github.com>
2023-03-17 10:28:51 -0400

    Sponsors v17

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { officers } from "../data";
+import { sponsors } from "../data";
 
 export default function Sponsors() {
   return (
@@ -11,9 +11,11 @@ export default function Sponsors() {
         </h1>
         <div className="container h-5 mx-auto flex px-10 py-5 "></div>
         <h1 className="title-font w-2/3 px-35 mx-auto sm:text-2xl text-lg mb-8 text-black font-semibold text-center ">
-          We are so happy you’re interested in getting involved with our work here at Truck Club at Virginia Tech.
-          There are so many ways for you to help, and we truly appreciate each and every effort. By lending your
-          support, you’ll become a valuable part of our Non-Profit Organization and help to strengthen our operations.
+          We are so happy you’re interested in getting involved with our work
+          here at Truck Club at Virginia Tech. There are so many ways for you to
+          help, and we truly appreciate each and every effort. By lending your
+          support, you’ll become a valuable part of our Non-Profit Organization
+          and help to strengthen our operations.
         </h1>
         <div className="container h-30 mx-auto flex px-10 py-5 "></div>
 
@@ -36,7 +38,8 @@ export default function Sponsors() {
               </h1>
               <a
                 href="https://forms.gle/QSobfhRWdqkX8Wi49"
-                className=" mx-auto inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-sm">
+                className=" mx-auto inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-sm"
+              >
                 <strong>Apply</strong>
               </a>
             </div>
@@ -61,7 +64,8 @@ export default function Sponsors() {
               </h1>
               <a
                 href="https://raw.githubusercontent.com/PrestonV78/TCVT-Site/main/public/sponsor_package.pdf"
-                className=" mx-auto inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-sm">
+                className=" mx-auto inline-flex text-black bg-white border-0 py-2 px-6 focus:outline-none hover:opacity-50 rounded text-sm"
+              >
                 <strong>Learn More</strong>
               </a>
             </div>
@@ -70,20 +74,19 @@ export default function Sponsors() {
           <div className="container h-10 mx-auto flex bg-white px-10 py-5 "></div>
         </div>
 
-        <div className="container w-full mx-auto flex flex-col bg-pink-900">
+        <div className="container w-full py-15 mx-auto flex flex-col bg-pink-900">
           <h1 className="title-font sm:text-5xl text-3xl text-white font-bold text-center">
             SPONSORSHIP PAYMENT
           </h1>
           <h1 className="title-font px-35 mx-35 sm:text-xl text-lg mb-8 text-white leading-relaxed text-center italic">
-            The Stripe processing fee of 2.9% + $0.30 has been added to fixed payments.
+            The Stripe processing fee of 2.9% + $0.30 has been added to fixed
+            payments.
           </h1>
           <h1 className="title-font px-35 mx-35 sm:text-xl text-lg mb-8 text-white font-semibold text-center italic">
-            Truck Club at Virginia Tech is a 501(c)3 organization. In short, you may be
-            able to claim a deduction on your federal taxes if you make a donation!
-            Payments to become an Official TCVT Sponsor are considered a donation to TCVT.
-            Our organization accepts cash, check, Venmo, or credit card via Stripe payments.
-            Please contact us via email at truckclubatvt@gmail.com if you have any questions
-            or concerns.
+            Payments to become an Official TCVT Sponsor are considered a
+            donation to TCVT. Our organization accepts cash, check, Venmo, or
+            credit card via Stripe payments. Please contact us via email at
+            truckclubatvt@gmail.com if you have any questions or concerns.
           </h1>
         </div>
 
@@ -100,27 +103,26 @@ export default function Sponsors() {
             Sponsor List
           </h1>
 
-          {officers.map((officer) => (
+          {sponsors.map((sponsor) => (
             <div className="container w-full lg:w-1/3 h-eqw my-4 flex relative">
               <img
                 alt="gallery"
                 className="absolute inset-0 h-full w-full object-contain object-center"
-                src={officer.image}
+                src={sponsor.image}
               />
               <div className="px-8 py-10 relative z-10 w-full h-full border-4 border-white bg-pink-900 opacity-0 hover:opacity-100">
                 <h2 className="tracking-widest text-sm title-font font-medium text-white mb-1">
-                  {officer.subtitle}
+                  {sponsor.subtitle}
                 </h2>
                 <h1 className="title-font text-lg font-medium text-white mb-3">
-                  {officer.title}
+                  {sponsor.title}
                 </h1>
                 <p className="leading-relaxed text-gray-400">
-                  {officer.description}
+                  {sponsor.description}
                 </p>
               </div>
             </div>
           ))}
-
         </div>
       </div>
     </section>
--- src/data.js
+++ src/data.js
@@ -71,53 +71,61 @@ export const officers = [
   },
 ];
 
-export const skills = [
-  "Java",
-  "Algorithms",
-  "C",
-  "Debugging",
-  "Python",
-  "Data Analysis",
-  "JavaScript",
-  "Figma",
-  "MATLAB",
-  "Problem Solving",
-  "React",
-  "GUI Modelling and Design",
-];
-
-export const courses = [
-  "Intermediate Software Design",
-  "Cryptography",
-  "Computer Systems",
-  "Differential Equations",
-  "Computer Organization",
-  "Linear Algebra",
-  "Problem Solving in CS",
-  "Discrete Mathematics",
-  "Software Design and Data Structures",
-  "Applied Combinatorics",
-  "Data Structures and Algorithms",
-  "Multivariable Calculus",
-  "Software Design",
-  "Single Variable Calculus",
-];
-
-export const experiences = [
+export const sponsors = [
   {
-    title: "Virginia Tech",
-    subtitle: "Education",
+    title: "LEONARD BUILDING  & TRUCK ACCESSORIES",
+    subtitle: "3930 S Main St., Blacksburg, VA 24060",
     description:
-      "Attended Virginia Polytechnic Instituate and State Universiry from July 2019 to May 2023. Graduating with a Bachelor's in Computer Science Engineering with a Minor in Mathematics.",
-    image: "./VirginiaTech.svg",
-    link: "mailto:https://vt.edu",
+    "Leonard remains committed to old-fashioned, looks you in the eye, and shakes your hand type of service. Our experienced and dedicated Team Members stand ready to exceed your expectations while providing quality products and services to meet your needs.",
+    image: "./Leonard.png",
+    },
+  {
+    title: "ERIN AND SHEILA REAL ESTATE TEAM",
+    subtitle: "erinandsheila.com",
+    description:
+    "Whether it’s time to sell or you’re looking at homes for sale in Blacksburg, homes for sale in Christiansburg, or anywhere else in the New River Valley, discover the power of two knowledgeable pros who know what it takes to get you the best deal possible.",    
+    image: "./Erin.webp",
   },
   {
-    title: "ThermoFlyAI",
-    subtitle: "GUI Team Lead",
+    title: "ARTHRITIS & OSTEOPOROSIS CENTER, LLC",
+    subtitle: "arthritisde.com",
     description:
-      "While attending Virginia Tech I participated in the ECE Major Design Experience class and was a part of the ThermoFlyAI team. I was the GUI/FrontEnd Team Lead on a project to develop an AI model to control a Residential HVAC System to allow for the learning of a User's preferred temperature based on a standard model and individual learning.",
-    image: "./ThermoFlyAI.svg",
-    link: "mailto:https://github.com/PrestonV78/ThermoFlyAIGUI",
+      "We provide compassionate and respectful care along with the leading techniques and therapies for patients with Rheumatic Diseases. We value and encourage all individuals’ rights to participate in their medical care with active choices and discussion with their caregivers.",
+    image: "./AOC.webp",
+  },
+  {
+    title: "KOUNTRY ROADS SOUNDS",
+    subtitle: "Facebook: Kountry Roads Sounds",
+    description:
+    "We are Kountry Roads Sounds. We offer music of all genres. Call us to book your next backyard party or big event. Reasonable priced.",    
+    image: "./krs_edited.webp",
+  },
+  {
+    title: "HIGH PEAK SPORTSWEAR",
+    subtitle: "209 College Ave, Blacksburg, VA 24060",
+    description:
+      "Let the friendly and knowledgeable staff of High Peak Sportswear Blacksburg make your next ordering experience a breeze. Whether your needs are custom screen printing, embroidery or some type of promotional product let our knowledge work for you. Our talented artists and production staff collectively have literally hundreds of years of experience, let us put what we’ve learned to work for you.",
+    image: "./highpeak.webp",
+  },
+  {
+    title: "MAD HATTER ENTERTAINMENT",
+    subtitle: "http://mhe-events.com/",
+    description:
+      "Mad Hatter Entertainment deals with festivals, comic conventions, car shows, event planning, and more!",
+    image: "./madhat.webp",
+  },
+  {
+    title: "NATIONAL AUTO SPORT ASSOCIATION - MID-ATLANTIC REGION",
+    subtitle: "https://nasaracing.net/",
+    description:
+      "The National Auto Sport Association (NASA) was formed in 1991 to deliver high-quality motorsports events to enthusiasts at major racing venues throughout the nation. NASA has created programs that allow owners of both racecars and high-performance street-driven vehicles to enjoy the full performance capabilities of their cars in a safe and controlled environment. NASA offers many different programs that will allow you to enjoy motorsports on a number of different levels, including our High-Performance Driving Events (HPDE), Time Trial, and Road Racing programs.",
+    image: "./nasa.webp",
+  },
+  {
+    title: "HYPERFEST",
+    subtitle: "https://www.hyper-fest.com/",
+    description:
+      "Haltech HyperFest presented by Grassroots Motorsports is the destination for those who want to be immersed in one of the premier automotive lifestyle events. With activities taking place on asphalt, dirt, mud, grass, and even in the air; we have you covered. From camping with your friends & family, riding with world class professional drifters, or enjoying music from a variety of artists, HyperFest offers numerous activities to enjoy both on and off the track over the course of an entire weekend.",
+    image: "./hf.webp",
   },
 ];
